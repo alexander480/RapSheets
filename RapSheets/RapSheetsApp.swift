d2b4112a83dba50dc8531524a1c7f51f007e5093
Alexander Lester <alexander480@gmail.com>
2022-09-19 00:16:49 -0400

    Initial Commit

---
+++ RapSheets/RapSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapSheetsApp.swift
+//  RapSheets
+//
+//  Created by Alexander Lester on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RapSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
